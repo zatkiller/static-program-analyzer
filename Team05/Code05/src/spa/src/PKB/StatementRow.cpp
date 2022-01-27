c1b3438441045275bcb4bee013e5e7ef8552336c
Teo Jun Xiong <teojunxiong97@gmail.com>
2022-01-28 00:33:10 +0800

    Swap content of StatementRow's h and cpp file

--- Team05/Code05/src/spa/src/PKB/StatementRow.cpp
+++ Team05/Code05/src/spa/src/PKB/StatementRow.cpp
@@ -1,5 +1,34 @@
 #pragma once
 
 #include <stdio.h>
+#include <vector>
+#include <set>
 
-#include "StatementRow.h"
+#include "../logging.h"
+#include "Row.h"
+#include "StatementType.h"
+
+class StatementRow : Row {
+public:
+	StatementRow(StatementType t, int l) : type(t), lineNumber(l) {}
+
+	bool equal(const StatementRow& row) {
+		if (type != row.type || lineNumber != row.lineNumber) {
+			return false;
+		}
+
+		return true;
+	}
+
+	bool operator == (const StatementRow& row) {
+		return equal(row);
+	}
+
+	bool operator < (const StatementRow& row) {
+		return !equal(row);
+	}
+
+private:
+	StatementType type;
+	int lineNumber;
+};
--- Team05/Code05/src/spa/src/PKB/StatementRow.h
+++ Team05/Code05/src/spa/src/PKB/StatementRow.h
@@ -8,42 +8,13 @@
 #include "Row.h"
 #include "StatementType.h"
 
-class StatementRow {
+class StatementRow: Row {
 public:
-	StatementRow(StatementType t, int l): type(t), lineNumber(l) {}
+	StatementRow(StatementType t, int l);
 
-	// TODO: replace <int> withh <PKBField>
-	bool contains(std::vector<int> queries) {
-		// Check if the number of queries matches the type of row. queries should have only 1 element
-		if (queries.size() == 0) {
-			Logger(Level::ERROR) << "StatementRow.contains() received an empty query.";
-			return false;
-		}
-		else if (queries.size() > 1) {
-			Logger(Level::ERROR) << "StatementRow.contains() received an invalid query.";
-			return false;
-		}
-		else {
-			// TODO: update based on PKBField implementation
-			// return queries.at(0).type == type && queries.at(0).v == lineNumber;
-		}
-	}
-
-	bool equal(const StatementRow& row) {
-		if (type != row.type || lineNumber != row.lineNumber) {
-			return false;
-		}
-
-		return true;
-	}
-
-	bool operator == (const StatementRow& row) {
-		return equal(row);
-	}
-
-	bool operator < (const StatementRow& row) {
-		return !equal(row);
-	}
+	bool equal(const StatementRow& row);
+	bool operator == (const StatementRow& row);
+	bool operator < (const StatementRow& row);
 
 private:
 	StatementType type;
