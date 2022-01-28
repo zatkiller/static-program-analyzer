b9ca9524cbe21bb8419469a0cc9ec65a9e3979b4
bailin <rkxuanfeng@gmail.com>
2022-01-28 18:26:38 +0800

    Add unit test for tokens and lexer

---
+++ Team05/Code05/src/spa/src/Lexer.cpp
@@ -0,0 +1,10 @@
+#include <Lexer.h>
+
+void Lexer::lex(std::string& source) {
+
+}
+
+std::queue<Token>& Lexer::getTokens() {
+    // TODO: insert return statement here
+    return tokens;
+}
---
+++ Team05/Code05/src/spa/src/Lexer.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include <string>
+#include <vector>
+#include <queue>
+#include <iostream>
+#include <variant>
+
+enum class TokenType {
+    eof,
+    name,    // [a-zA-Z][a-zA-Z0-9]*
+    number,  // [0-9]+
+    special, // special characters like +-*/><=;
+};
+
+struct Token {
+    const TokenType type;
+    const std::variant<char, std::string, int> value;
+
+    bool operator==(const Token& o) const {
+        return type == o.type && value == o.value;
+    }
+};
+
+
+class Lexer {
+private:
+    std::queue<Token> tokens;
+    void lex(std::string& source);
+public:
+    Lexer(std::string& source) {
+        this->lex(source);
+    }
+    std::queue<Token>& getTokens();
+};
