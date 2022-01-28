77ae6a50f61c769d1290c8b635f14c3eda9af3dc
Teo Jun Xiong <teojunxiong97@gmail.com>
2022-01-28 16:36:40 +0800

    Fix RelationshipRow function definitions

--- Team05/Code05/src/spa/src/PKB/RelationshipRow.cpp
+++ Team05/Code05/src/spa/src/PKB/RelationshipRow.cpp
@@ -2,20 +2,14 @@
 
 #include <stdio.h>
 
-class RelationshipRow {
-public:
-	RelationshipRow(int e1, int e2): entity1(e1), entity2(e2) {};
+#include "RelationshipRow.h"
 
-	bool operator == (const RelationshipRow& row) const {
-		return entity1 == row.entity1 && entity2 == row.entity2;
-	};
+RelationshipRow::RelationshipRow(int e1, int e2): entity1(e1), entity2(e2) {};
 
-	bool operator < (const RelationshipRow& row) const {
-		return !(entity1 == row.entity1 && entity2 == row.entity2);
-	};
+bool RelationshipRow::operator == (const RelationshipRow& row) const {
+	return entity1 == row.entity1 && entity2 == row.entity2;
+};
 
-private:
-	// TODO: change to PKBField
-	int entity1;
-	int entity2;
+bool RelationshipRow::operator < (const RelationshipRow & row) const {
+	return !(entity1 == row.entity1 && entity2 == row.entity2);
 };
