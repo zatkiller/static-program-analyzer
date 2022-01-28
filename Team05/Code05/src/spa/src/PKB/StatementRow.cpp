2d98df264d93d7c86b3bd0c476956f29318b2e78
Teo Jun Xiong <teojunxiong97@gmail.com>
2022-01-28 16:38:33 +0800

    Fix StatementRow function definitions

--- Team05/Code05/src/spa/src/PKB/StatementRow.cpp
+++ Team05/Code05/src/spa/src/PKB/StatementRow.cpp
@@ -2,21 +2,14 @@
 
 #include <stdio.h>
 
-#include "StatementType.h"
+#include "StatementRow.h"
 
-class StatementRow {
-public:
-	StatementRow(StatementType t, int l) : type(t), lineNumber(l) {}
+StatementRow::StatementRow(StatementType t, int l) : type(t), lineNumber(l) {}
 
-	bool operator == (const StatementRow& row) const {
-		return type == row.type && lineNumber == row.lineNumber;
-	}
+bool StatementRow::operator == (const StatementRow& row) const {
+	return type == row.type && lineNumber == row.lineNumber;
+}
 
-	bool operator < (const StatementRow& row) const {
-		return !(type == row.type && lineNumber == row.lineNumber);
-	}
-
-private:
-	StatementType type;
-	int lineNumber;
-};
+bool StatementRow::operator < (const StatementRow& row) const {
+	return !(type == row.type && lineNumber == row.lineNumber);
+}
