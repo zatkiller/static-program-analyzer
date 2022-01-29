f42e1f0b18e935e43e3b0afc5e83a213d9dcfdcb
zatkiller <gheedylan@gmai.com>
2022-01-30 03:09:32 +0800

    add Lexer getNextToken and getNextReservedToken

--- Team05/Code05/src/spa/src/pql/lexer.cpp
+++ Team05/Code05/src/spa/src/pql/lexer.cpp
@@ -1,11 +1,82 @@
 #include "lexer.h"
 
-#include <sstream>
+void Lexer::eatWhitespace() {
+    while (text.length() > 0 && (text[0] == ' ' || text[0] == '\n'))
+        text.erase(0, 1);
+}
 
-Lexer::Lexer(std::string input) {
-    std::stringstream ss(input);
-    std::string token;
-    while (ss >> token) {
-        tokens.push_back(token);
+bool Lexer::isLetter(char c) {
+    bool isLower = 'a' <= c && c <= 'z';
+    bool isUpper = 'A' <= c && c <= 'Z';
+    return isLower || isUpper;
+}
+
+bool Lexer::isDigit(char c) {
+    return '0' <= c && c <= '9';
+}
+
+bool Lexer::hasPrefix(std::string prefix) {
+    return text.find(prefix) == 0;
+}
+
+Token Lexer::getNextToken() {
+    eatWhitespace();
+
+    if (text.length() == 0) {
+        return Token{"", TokenType::Eof};
+    } else if (isLetter(text[0])) {
+        int charCount = 0;
+        while (text.length() > 0 && (isLetter(text[charCount]) || isDigit(text[charCount])))
+            charCount++;
+
+        std::string identifier = text.substr(0, charCount);
+        text.erase(0, charCount);
+
+        return Token { identifier, TokenType::Identifier};
+    } else if (isDigit(text[0])) {
+        int charCount = 0;
+        while (text.length() > 0 && (isDigit(text[charCount])))
+            charCount++;
+
+        std::string number = text.substr(charCount);
+        text.erase(0, charCount);
+
+        return Token { number, TokenType::Number};
+    } else {
+        Token token;
+        std::string value = text.substr(0, 1);
+        if (text[0] == ';') {
+            token = {value, TokenType::Semicolon};
+        } else if (text[0] == '(') {
+            token = {value, TokenType::OpeningParan};
+        } else if (text[0] == ')') {
+            token = {value, TokenType::ClosingParan};
+        } else if (text[0] == '_') {
+            token = {value, TokenType::Underscore};
+        } else if (text[0] == ',') {
+            token = {value, TokenType::Comma};
+        } else {
+            token = {"", TokenType::Invalid};
+        }
+
+        text.erase(0, 1);
+        return token;
     }
 }
+
+Token Lexer::getNextReservedToken() {
+    eatWhitespace();
+
+    if (hasPrefix("Select")) {
+        return Token {"Select", TokenType::Select};
+    } else if (hasPrefix("Modifies")) {
+        return Token {"Modifies", TokenType::Modifies};
+    } else if (hasPrefix("Uses")) {
+        return Token {"Uses", TokenType::Uses};
+    } else if (hasPrefix("pattern")) {
+        return Token {"pattern", TokenType::Pattern};
+    } else {
+        return Token {"", TokenType::Invalid};
+    }
+}
+
--- Team05/Code05/src/spa/src/pql/lexer.h
+++ Team05/Code05/src/spa/src/pql/lexer.h
@@ -1,10 +1,68 @@
 #include <string>
 #include <vector>
 
-class Lexer {
-    std::vector<std::string> tokens;
+enum class TokenType {
+    Eof,
+    Invalid,
 
-    Lexer(std::string);
+    Underscore,
+    Semicolon,
+    Comma,
+    Period,
+    OpeningParan,
+    ClosingParan,
+
+    Identifier,
+    Number,
+    String,
+
+    // Relationships
+    Uses,
+    Modifies,
+
+    // Reserved Keywords
+    Select,
+    SuchThat,
+    Pattern,
+    Assign,
+
+    //Relationship variables
+    Value,
+    StmtNum,
+    VarName,
+    ProcName
+};
+
+struct Token {
+    std::string text;
+    TokenType type;
+
+    TokenType getTokenType() {
+        return this->type;
+    }
+
+    std::string getText() {
+        return this->text;
+    }
+
+    bool operator==(const Token &o) const {
+        return (this->type == o.type) && (this->text == o.text);
+    }
+};
+
+struct Lexer {
+    std::string text;
+
+    Lexer(std::string input) : text(input) {};
+
+    void eatWhitespace();
+
+    bool isLetter(char c);
+    bool isDigit(char c);
+    bool hasPrefix(std::string prefix);
+
+    Token getNextToken();
+    Token getNextReservedToken();
 };
 
 
