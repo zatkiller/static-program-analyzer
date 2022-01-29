5d82c387422ee0c972759dbe2b3b89b62b48cb58
zatkiller <gheedylan@gmai.com>
2022-01-30 03:32:05 +0800

    add Lexer eatwhitespace testcase

--- Team05/Code05/src/unit_testing/src/pql/test_lexer.cpp
+++ Team05/Code05/src/unit_testing/src/pql/test_lexer.cpp
@@ -2,18 +2,50 @@
 #include "catch.hpp"
 #include "logging.h"
 
-TEST_CASE("Test PQL Lexer") {
+TEST_CASE("Lexer tokenisation") {
     std::string testQuery = "assign a; variable v;\n Select a such that Uses (a, v) pattern a (v, _)";
 
     Lexer lexer(testQuery);
 
-    REQUIRE(lexer.getNextToken() == Token { "assign", TokenType::Identifier});
-    REQUIRE(lexer.getNextToken() == Token { "a", TokenType::Identifier});
+    REQUIRE(lexer.getNextToken() == Token{"assign", TokenType::Identifier});
+    REQUIRE(lexer.getNextToken() == Token{"a", TokenType::Identifier});
     REQUIRE(lexer.getNextToken().getText() == ";");
 
-    REQUIRE(lexer.getNextToken() == Token { "variable", TokenType::Identifier});
-    REQUIRE(lexer.getNextToken() == Token { "v", TokenType::Identifier});
+    REQUIRE(lexer.getNextToken() == Token{"variable", TokenType::Identifier});
+    REQUIRE(lexer.getNextToken() == Token{"v", TokenType::Identifier});
     REQUIRE(lexer.getNextToken().getText() == ";");
 
-    REQUIRE(lexer.getNextReservedToken() == Token { "Select", TokenType::Select});
+    REQUIRE(lexer.getNextReservedToken() == Token{"Select", TokenType::Select});
+    REQUIRE(lexer.getNextToken() == Token{"a", TokenType::Identifier});
+
+    REQUIRE(lexer.getNextReservedToken() == Token{"such that", TokenType::SuchThat});
+    REQUIRE(lexer.getNextReservedToken() == Token{"Uses", TokenType::Uses});
+
+    REQUIRE(lexer.getNextToken() == Token{"(", TokenType::OpeningParan});
+    REQUIRE(lexer.getNextToken() == Token{"a", TokenType::Identifier});
+    REQUIRE(lexer.getNextToken() == Token{",", TokenType::Comma});
+    REQUIRE(lexer.getNextToken() == Token{"v", TokenType::Identifier});
+    REQUIRE(lexer.getNextToken() == Token{")", TokenType::ClosingParan});
+
+    REQUIRE(lexer.getNextReservedToken() == Token{"pattern", TokenType::Pattern});
+    REQUIRE(lexer.getNextToken() == Token{"a", TokenType::Identifier});
+
+    REQUIRE(lexer.getNextToken() == Token{"(", TokenType::OpeningParan});
+    REQUIRE(lexer.getNextToken() == Token{"v", TokenType::Identifier});
+    REQUIRE(lexer.getNextToken() == Token{",", TokenType::Comma});
+    REQUIRE(lexer.getNextToken() == Token{"_", TokenType::Underscore});
+    REQUIRE(lexer.getNextToken() == Token{")", TokenType::ClosingParan});
+}
+
+TEST_CASE("Lexer eatWhitespace") {
+    std::string string1 = "   ";
+    std::string string2 = "\n";
+
+    Lexer lexer1(string1);
+    lexer1.eatWhitespace();
+    REQUIRE(lexer1.text.length() == 0);
+
+    Lexer lexer2(string2);
+    lexer2.eatWhitespace();
+    REQUIRE(lexer2.text.length() == 0);
 }
