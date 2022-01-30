be6fdca4bbb17194342da9affb61d8cdeda9b89d
Nay Lin Han <jamesliu09@gmail.com>
2022-01-30 17:18:51 +0800

    Add AST

---
+++ Team05/Code05/src/spa/src/Parser/AST.h
@@ -0,0 +1,333 @@
+#pragma once
+
+#include <string>
+#include <vector>
+#include <memory>
+
+namespace AST {
+
+	#define StmtLst std::vector<std::unique_ptr<Statement>>
+	
+	class Procedure;
+	class Statement;
+	class Expr;
+	class CondExpr;
+	class BinExpr;
+	class Var;
+	class Const;
+
+	class ASTNode {
+	public:
+		// TODO: visiter method to be added
+		virtual ~ASTNode() {}
+	};
+
+	/**
+	 * Represents the Program in the AST.
+	 * Program contains a single Procedure.
+	 * Identified by:
+	 *  procedure
+	 */
+	class Program : public ASTNode {
+	private:
+		std::unique_ptr<Procedure> procedure;
+	public: 
+		Program(std::unique_ptr<Procedure> procedure) :
+			procedure(std::move(procedure)) {}
+	};	
+
+	/**
+	 * Represents a procedure in the AST.
+	 * Identified by: 
+	 *  'procedure' procName stmtlst
+	 */
+	class Procedure : public ASTNode {
+	private:
+		std::string procName;
+		StmtLst stmtLst;
+	public:
+		Procedure(const std::string& procName, 
+			StmtLst stmtLst) :
+			procName(procName), stmtLst(std::move(stmtLst)) {}
+	};
+
+	/**
+	 * Represents a statement in the AST.
+	 * Base class for: 'read', 'print', 'while', 'if' and 'assign' statements.
+	 * Identified by:
+	 *  read | print | while | if | assign
+	 */
+	class Statement : public ASTNode {
+	private:
+		int stmtNo;
+	public:
+		~Statement() {}
+		Statement(int stmtNo) : stmtNo(stmtNo) {}
+	};
+
+	/**
+	 * Represents an 'if' statement in the AST.
+	 * Identified by:
+	 *  'if' '(' cond_expr ')' 'then' '{' stmtLst '}' 'else' '{' stmtLst '}'
+	 */
+	class If : public Statement {
+	private:
+		std::unique_ptr<CondExpr> condExpr;
+		StmtLst thenBlk;
+		StmtLst elseBlk;
+	public:
+		If(
+			int stmtNo,
+			std::unique_ptr<CondExpr> condExpr,
+			StmtLst thenBlk,
+			StmtLst elseBlk
+		) :
+			Statement(stmtNo),
+			condExpr(std::move(condExpr)),
+			thenBlk(std::move(thenBlk)),
+			elseBlk(std::move(elseBlk)) {}
+	};
+
+	/**
+	 * Represents an 'while' statement in the AST.
+	 * Identified by:
+	 *  'while' '(' cond_expr ')' '{' stmtLst '}'
+	 */
+	class While : public Statement {
+	private:
+		std::unique_ptr<CondExpr> condExpr;
+		StmtLst stmtLst;
+	public:
+		While(
+			int stmtNo,
+			std::unique_ptr<CondExpr> condExpr,
+			StmtLst stmtLst
+		) :
+			Statement(stmtNo),
+			condExpr(std::move(condExpr)),
+			stmtLst(std::move(stmtLst)) {}
+	};
+
+	/**
+	 * Represents an 'assign' statement in the AST.
+	 * Identified by:
+	 *  var_name '=' expr ';'
+	 */
+	class Assign : public Statement {
+	private:
+		std::unique_ptr<Var> var;
+		std::unique_ptr<Expr> expr;
+	public:
+		Assign(
+			int stmtNo,
+			std::unique_ptr<Var> var, 
+			std::unique_ptr<Expr> expr
+		) :
+			Statement(stmtNo),
+			var(std::move(var)), 
+			expr(std::move(expr)) {}
+	};
+
+	/**
+	 * Represents an input/output statement in the AST.
+	 * Base class for 'Read and 'Print' statements.
+	 */
+	class IO : public Statement {
+	private:
+		std::unique_ptr<Var> var;
+	public:
+		IO(
+			int stmtNo, 
+			std::unique_ptr<Var> var
+		) : 
+			Statement(stmtNo),
+			var(std::move(var)) {}
+	};
+
+	/**
+	 * Represents a Read statement in the AST.
+	 * Identified by:
+	 *  'read' var_name;
+	 */
+	class Read : public IO {
+		using IO::IO;
+	};
+
+	/**
+	 * Represents a Print statement in the AST.
+	 * Identified by:
+	 *  'print' var_name;
+	 */
+	class Print : public IO {
+		using IO::IO;
+	};
+
+	/**
+	 * Base class for Var/Const/BinExpr in the AST.
+	 * Identified by:
+	 *  expr | term | factor
+	 * where:
+	 *  expr: expr ‘+’ term | expr ‘-’ term | term
+	 *  term: term ‘*’ factor | term ‘/’ factor | term ‘%’ factor | factor
+	 *  factor: var_name | const_value | ‘(’ expr ‘)’
+	 */
+	class Expr : public ASTNode {
+	public:
+		virtual ~Expr() {}
+	};
+
+	/**
+	 * Represents a variable in the AST.
+	 * Identified by:
+	 *  var_name
+	 */
+	class Var : public Expr {
+	private:
+		std::string varName;
+	public:
+		Var(const std::string& varName) : varName(varName) {};
+		std::string& getVarName() { return varName; }
+	};
+
+	/**
+	 * Represents a const in the AST.
+	 * Identified by:
+	 *  const_value
+	 */
+	class Const : public Expr {
+	private:
+		int constValue;
+	public:
+		Const(int constValue) : constValue(constValue) {};
+	};
+
+	/**
+	 * Represents possible operators in BinExpr.
+	 */
+	enum class BinOp {
+		PLUS   = '+',
+		MINUS  = '-',
+		DIVIDE = '/',
+		MULT   = '*',
+		MOD    = '%'
+	};
+
+	/**
+	 * Represents an expression with 2 Expr and an operator in the AST.
+	 * Identified by:
+	 *  from expr: expr '+' term | expr '-' term |
+	 *  from term: term '*' factor | term '/' factor | term '%' factor
+	 */
+	class BinExpr: public Expr {
+	private:
+		BinOp Op;
+		std::unique_ptr<Expr> LHS, RHS;
+	public:
+		BinExpr(
+			BinOp Op, 
+			std::unique_ptr<Expr> LHS, 
+			std::unique_ptr<Expr> RHS
+		) : 
+			Op(Op), 
+			LHS(std::move(LHS)), 
+			RHS(std::move(RHS)) {}
+	};
+
+	/**
+	 * Represents a conditional expression in the AST
+	 * Identified by:
+	 *  cond_expr | rel_expr | rel_factor
+	 * where:
+	 *  cond_expr: rel_expr | '!' '(' cond_expr ')' |
+	 *    '(' cond_expr ')' '&&' '(' cond_expr ')' |
+	 *    '(' cond_expr ')' '||' '(' cond_expr ')'
+	 *  rel_expr: rel_factor '>' rel_factor | rel_factor '>=' rel_factor | 
+	 *    rel_factor '<' rel_factor | rel_factor '<' rel_factor | 
+	 *    rel_factor '==' rel_factor | rel_factor '!=' rel_factor
+	 *  rel_factor: var_name | const_value | expr
+	 */
+	class CondExpr : public ASTNode {
+	public:
+		virtual ~CondExpr() {}
+	};
+
+	/**
+	 * Represents the possible operators in RelExpr
+	 */
+	enum class RelOp {
+		LTE,  // <=
+		GTE,  // >=
+		LT,   // <
+		GT,   // >
+		EQ,   // ==
+		NE    // !=
+	};
+
+	/**
+	 * Represents a RelExpr in the AST.
+	 * Identified by: 
+	 *  rel_expr: rel_factor '>' rel_factor | rel_factor '>=' rel_factor | 
+	 *    rel_factor '<' rel_factor | rel_factor '<' rel_factor | 
+	 *    rel_factor '==' rel_factor | rel_factor '!=' rel_factor
+	 */
+	class RelExpr : public CondExpr {
+	private:
+		RelOp Op;
+		std::unique_ptr<Expr> LHS, RHS;
+	public:
+		RelExpr(
+			RelOp Op, 
+			std::unique_ptr<Expr> LHS, 
+			std::unique_ptr<Expr> RHS
+		) : 
+			Op(Op), 
+			LHS(std::move(LHS)), 
+			RHS(std::move(RHS)) {}
+	};
+
+	/**
+	 * Represents the possible operators in CondBinExpr and NotCondExpr
+	 */
+	enum class CondOp {
+		NOT, // !
+		AND, // &&
+		OR   // ||
+	};
+	/**
+	 * Represents CondBinExpr in the AST.
+	 * Identified by:
+	 *  from cont_expr: ‘(’ cond_expr ‘)’ ‘&&’ ‘(’ cond_expr ‘)’ |
+	 *    ‘(’ cond_expr ‘)’ ‘||’ ‘(’ cond_expr ‘)’
+	 */
+	class CondBinExpr : public CondExpr {
+	private:
+		CondOp Op;
+		std::unique_ptr<CondExpr> LHS, RHS;
+	public:
+		CondBinExpr(
+			CondOp Op, 
+			std::unique_ptr<CondExpr> LHS, 
+			std::unique_ptr<CondExpr> RHS
+		) : 
+			Op(Op), 
+			LHS(std::move(LHS)), 
+			RHS(std::move(RHS)) {}
+	};
+
+	/**
+	 * Represents the condition expression with '!' in the AST.
+	 * Identified by:
+	 *   from cont_expr: ‘!’ ‘(’ cond_expr ‘)’
+	 */
+	class NotCondExpr : public CondExpr {
+	private:
+		CondOp Op = CondOp::NOT;
+		std::unique_ptr<CondExpr> condExpr;
+	public:
+		NotCondExpr(
+			std::unique_ptr<CondExpr> condExpr
+		) : 
+			condExpr(std::move(condExpr)) {}
+	};
+
+}
--- Team05/Code05/src/unit_testing/src/TestAST.cpp
+++ Team05/Code05/src/unit_testing/src/TestAST.cpp
@@ -1,19 +1,96 @@
 
 #include "TNode.h"
+#include "Parser/AST.h"
+#include <iostream>
+#include <vector>
 
 #include "catch.hpp"
-using namespace std;
+
+using namespace AST;
+
 void require(bool b) {
     REQUIRE(b);
 }
 
-TEST_CASE("1st Test") {
+// https://github.com/catchorg/Catch2/blob/devel/docs/tutorial.md
 
-    TNode T;
-	
-	
-	
-    require(1 == 1);
+TEST_CASE("AST Test") {
+    // Construction test
+    INFO("Constructing the AST");
+    
+    auto v1 = std::make_unique<Var>("v1");
+    auto v2 = std::make_unique<Var>("v2");
+    auto v3 = std::make_unique<Var>("v3");
+    auto v4 = std::make_unique<Var>("v4");
+    auto v5 = std::make_unique<Var>("v5");
+    auto v6 = std::make_unique<Var>("v6");
+
+    auto one = std::make_unique<Const>(1);
+    auto constVal10 = std::make_unique<Const>(10);
+    auto constVal5 = std::make_unique<Const>(5);
+
+    SECTION("Assign construction") {
+        auto assign = std::make_unique<Assign>(1, std::move(v1), std::move(v2)); // v1 = v2
+    };
+
+    SECTION("Binary Expression construction") {
+        // v1 + v2
+        auto binExpr1 = std::make_unique<AST::BinExpr>(AST::BinOp::PLUS, std::move(v1), std::move(v2));
+        // 10 - 5
+        auto binExpr2 = std::make_unique<AST::BinExpr>(AST::BinOp::MINUS, std::move(constVal10), std::move(constVal5));
+        // (v1 + v2) + (10 - 5)
+        auto binExpr3 = std::make_unique<AST::BinExpr>(AST::BinOp::PLUS, std::move(binExpr1), std::move(binExpr2));
+    };
+
+    SECTION("Condition Expression construction") {
+        // 10 > 5
+        auto relExpr1 = std::make_unique<RelExpr>(RelOp::GT, std::move(constVal10), std::move(constVal5));
+        // v1 == v2
+        auto relExpr2 = std::make_unique<RelExpr>(RelOp::EQ, std::move(v1), std::move(v2));
+        // (10 > 5) && (v1 == v2) 
+        auto condExpr = std::make_unique<CondBinExpr>(CondOp::AND, std::move(relExpr1), std::move(relExpr2));
+    };
+
+    SECTION("IO Statements Construction") {
+        // read
+        auto readStmt = std::make_unique<Read>(20, std::move(v1));
+        auto readStmt2 = std::make_unique<Read>(69, std::move(v2));
+        // print
+        auto printStmt = std::make_unique<Print>(21, std::move(v3));
+    }
+    
+    SECTION("Statement Construction") {
+        auto readStmt = std::make_unique<Read>(20, std::move(v1));
+        auto readStmt2 = std::make_unique<Read>(69, std::move(v2));
+        auto printStmt = std::make_unique<Print>(21, std::move(v3));
+
+        // while ((10 > 5) && (v1 == v2)) {
+        //     read v1;
+        //     print v3;
+        // }
+        auto relExpr1 = std::make_unique<RelExpr>(RelOp::GT, std::move(constVal10), std::move(constVal5));
+        auto relExpr2 = std::make_unique<RelExpr>(RelOp::EQ, std::move(v4), std::move(v5));
+        auto condExpr = std::make_unique<CondBinExpr>(CondOp::AND, std::move(relExpr1), std::move(relExpr2));
+        auto vect = std::vector<std::unique_ptr<Statement>>();
+        vect.push_back(std::move(readStmt));
+        vect.push_back(std::move(printStmt));
+        auto whileStmt = std::make_unique<While>(22, std::move(condExpr), std::move(vect));
+        // if ((10 > 5) && (v1 == v2)) then {
+        //     read v2;
+        // } else {
+        // }
+        auto vect2 = std::vector<std::unique_ptr<Statement>>();
+        vect2.push_back(std::move(readStmt2));
+        auto vect3 = std::vector<std::unique_ptr<Statement>>();
+        auto ifStmt = std::make_unique<If>(80, std::move(condExpr), std::move(vect2), std::move(vect3));
+        // assign
+        auto assignStmt = std::make_unique<Assign>(42, std::move(v6), std::make_unique<Const>(69));
+    }
+
+    SUCCEED("No errors thrown during object construction");
+    // Access modifier test
+
+    // require(1 == 1);
 }
 
 
