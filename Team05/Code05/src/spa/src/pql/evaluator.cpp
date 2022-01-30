0b32769bb0ed7d0f480e76adb4b0f65f48c85780
YangYue128-helen <yangyue1228@hotmail.com>
2022-01-30 22:28:19 +0800

    add evaluator

---
+++ Team05/Code05/src/spa/src/pql/evaluator.cpp
@@ -0,0 +1,4 @@
+//
+// Created by 杨悦Helen on 30/1/22.
+//
+
---
+++ Team05/Code05/src/spa/src/pql/evaluator.h
@@ -0,0 +1,8 @@
+//
+// Created by 杨悦Helen on 30/1/22.
+//
+
+#ifndef INC_21S2_CP_SPA_TEAM_05_EVALUATOR_H
+#define INC_21S2_CP_SPA_TEAM_05_EVALUATOR_H
+
+#endif //INC_21S2_CP_SPA_TEAM_05_EVALUATOR_H
