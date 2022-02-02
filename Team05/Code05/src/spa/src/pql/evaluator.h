e8413cbff53cbb1e4b89de4760ffe3ff0897d00f
YangYue128-helen <yangyue1228@hotmail.com>
2022-02-02 17:25:14 +0800

    chang to shared_ptr for suchthat

--- Team05/Code05/src/spa/src/pql/evaluator.h
+++ Team05/Code05/src/spa/src/pql/evaluator.h
@@ -8,7 +8,7 @@
 #include <memory>
 #include "query.h"
 
-std::set<int> processSuchthat(std::vector<RelRef>, DesignEntity);
+std::set<int> processSuchthat(std::vector<std::shared_ptr<RelRef>>, DesignEntity);
 
 std::set<int> processPattern(std::vector<Pattern>, DesignEntity);
 
