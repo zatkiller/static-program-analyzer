66bc106df5bdaf9f079558d07d696bd635ac220c
YangYue128-helen <yangyue1228@hotmail.com>
2022-02-02 14:44:59 +0800

    add methods into head file

--- Team05/Code05/src/spa/src/pql/evaluator.h
+++ Team05/Code05/src/spa/src/pql/evaluator.h
@@ -2,7 +2,12 @@
 #include <unordered_map>
 #include <vector>
 #include <stdexcept>
+#include <set>
 
 #include <query.h>
 
-std::string evaluate(Query query);
+std::string evaluate(Query);
+
+std::set<int> processSuchthat(std::vector<RelRef>, DesignEntity);
+
+std::set<int> processPattern(std::vector<Pattern>, DesignEntity);
