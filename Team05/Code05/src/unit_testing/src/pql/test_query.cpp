67c714a05fef604514b2d017079033a1e0c0cdc5
zatkiller <gheedylan@gmail.com>
2022-02-04 16:44:40 +0800

    add unit testing for StmtRef

--- Team05/Code05/src/unit_testing/src/pql/test_query.cpp
+++ Team05/Code05/src/unit_testing/src/pql/test_query.cpp
@@ -1,6 +1,27 @@
 #include "pql/query.h"
 #include "catch.hpp"
 
+TEST_CASE("StmtRef") {
+    StmtRef stmtRef;
+    REQUIRE(stmtRef.getType() == StmtRefType::NOT_INITIALIZED);
+
+    StmtRef stmtRef1;
+    stmtRef1.setType(StmtRefType::LINE_NO);
+    stmtRef1.setLineNo(1);
+    REQUIRE(stmtRef1.isLineNo());
+    REQUIRE(stmtRef1.getLineNo() == 1);
+
+    StmtRef stmtRef2;
+    stmtRef2.setType(StmtRefType::WILDCARD);
+    REQUIRE(stmtRef2.isWildcard());
+
+    StmtRef stmtRef3;
+    stmtRef3.setType(StmtRefType::DECLARATION);
+    stmtRef3.setDeclaration("a");
+    REQUIRE(stmtRef3.isDeclaration());
+    REQUIRE(stmtRef3.getDeclaration() == "a");
+}
+
 TEST_CASE("Query hasDeclaration") {
     Query query = Query{};
     REQUIRE(!query.hasDeclaration("a"));
