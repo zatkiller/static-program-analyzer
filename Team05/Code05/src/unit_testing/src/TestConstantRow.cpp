154c6d07e70ff4363c6aa05ba7a0859a8390ab56
Patrick <patricktan9955@hotmail.com>
2022-02-06 06:30:32 +0800

    Add tests for ConstantRow and ConstantTable

---
+++ Team05/Code05/src/unit_testing/src/TestConstantRow.cpp
@@ -0,0 +1,19 @@
+#include "pkb/ConstantRow.h"
+#include "logging.h"
+#include <iostream>
+
+#include "catch.hpp"
+
+#define TEST_LOG Logger() << "TestConstantRow.cpp"
+
+TEST_CASE("ConstantRow operator testing") {
+	ConstantRow row1{ CONST{1} };
+	ConstantRow row2{ CONST{1} };
+	ConstantRow row3{ CONST{2} };
+
+	TEST_LOG << "Test ConstantRow ==";
+	REQUIRE(row1 == row2);
+	REQUIRE(row1 == row1);
+
+	REQUIRE_FALSE(row1 == row3);
+}
