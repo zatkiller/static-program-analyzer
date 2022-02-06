b12e2adb2de591a3770168cb1b14923f60acda68
bailin <rkxuanfeng@gmail.com>
2022-02-06 16:02:57 +0800

    Remove additional qualifiers to build on unix

--- Team05/Code05/src/spa/src/Parser/Parser.h
+++ Team05/Code05/src/spa/src/Parser/Parser.h
@@ -21,15 +21,15 @@ private:
     std::unique_ptr<AST::Expr> shuntingYardParser(std::deque<Token>& tokens);
     std::unique_ptr<AST::CondExpr> parseRelExpr(std::deque<Token>& tokens);
     std::unique_ptr<AST::CondExpr> parseCondExpr(std::deque<Token>& tokens);
-    std::unique_ptr<AST::Expr> Parser::parseExpr(std::deque<Token>& tokens);
-    AST::StmtLst Parser::parseStmtLst(std::deque<Token>& tokens);
+    std::unique_ptr<AST::Expr> parseExpr(std::deque<Token>& tokens);
+    AST::StmtLst parseStmtLst(std::deque<Token>& tokens);
     std::unique_ptr<AST::Statement> parseReadStmt(std::deque<Token>& tokens);
-    std::unique_ptr<AST::Statement> Parser::parsePrintStmt(std::deque<Token>& tokens);
-    std::unique_ptr<AST::Statement> Parser::parseAssignStmt(std::deque<Token>& tokens);
-    std::unique_ptr<AST::Statement> Parser::parseWhileStmt(std::deque<Token>& tokens);
-    std::unique_ptr<AST::Statement> Parser::parseIfStmt(std::deque<Token>& tokens);
-    std::unique_ptr<AST::Procedure> Parser::parseProcedure(std::deque<Token>& tokens);
-    std::unique_ptr<AST::Program> Parser::parseProgram(std::deque<Token>& tokens);
+    std::unique_ptr<AST::Statement> parsePrintStmt(std::deque<Token>& tokens);
+    std::unique_ptr<AST::Statement> parseAssignStmt(std::deque<Token>& tokens);
+    std::unique_ptr<AST::Statement> parseWhileStmt(std::deque<Token>& tokens);
+    std::unique_ptr<AST::Statement> parseIfStmt(std::deque<Token>& tokens);
+    std::unique_ptr<AST::Procedure> parseProcedure(std::deque<Token>& tokens);
+    std::unique_ptr<AST::Program> parseProgram(std::deque<Token>& tokens);
 
     friend class ParserUnitTest;
 };
