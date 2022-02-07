0ddf7043f6109cbd37a5d6392771c20c469ecd73
Teo Jun Xiong <teojunxiong97@gmail.com>
2022-02-07 14:33:00 +0800

    Add documentation for ProcedureRow, ProcedureTable, RelationshipRow, RelationshipTable

--- Team05/Code05/src/spa/src/PKB/ProcedureRow.h
+++ Team05/Code05/src/spa/src/PKB/ProcedureRow.h
@@ -5,17 +5,28 @@
 
 #include "PKBDataTypes.h"
 
+/**
+* A data structure to store procedure names (strings) in a ProcedureTable.
+*/
 class ProcedureRow {
 public:
     explicit ProcedureRow(PROC_NAME);
-
-    bool operator == (const ProcedureRow&) const;
+    
+    /**
+    * Retrieves the procedure name stored in the ProcedureRow.
+    *
+    * @return a procedure name wrapped in PROC_NAME
+    */
     PROC_NAME getProcName() const;
+    bool operator == (const ProcedureRow&) const;
 
 private:
     PROC_NAME procedureName;
 };
 
+/**
+* Hash function for ProcedureRow.
+*/
 class ProcedureRowHash {
     public:
         size_t operator() (const ProcedureRow&) const;
