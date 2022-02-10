e9824e402d23e8e0f476891ad0b0c9a9ebf9acc1
Teo Jun Xiong <teojunxiong97@gmail.com>
2022-02-11 01:14:24 +0800

    Add tests for TestConstant*

--- Team05/Code05/src/unit_testing/src/TestConstantTable.cpp
+++ Team05/Code05/src/unit_testing/src/TestConstantTable.cpp
@@ -7,23 +7,38 @@
 
 #define TEST_LOG Logger() << "TestConstantTable.cpp"
 
-TEST_CASE("ConstantTable testing") {
+TEST_CASE("ConstantTable insert and contains") {
     ConstantTable table{};
-
-    PKBField field1 = PKBField::createConcrete(CONST{1});
-    PKBField field2 = PKBField::createConcrete(CONST{2});
-    PKBField field3 = PKBField::createConcrete(CONST{1});
-
-    TEST_LOG << "Test empty ConstantTable#contains";
+    PKBField field1 = PKBField::createConcrete(1);
+    PKBField field2 = PKBField::createConcrete(2);
+    PKBField field3 = PKBField::createConcrete(1);
     REQUIRE_FALSE(table.contains(field1));
 
-    TEST_LOG << "Test duplicate ConstantTable#insert";
     table.insert(field1);
     table.insert(field3);
-    REQUIRE(table.getSize() == 1);
+    REQUIRE(table.contains(PKBField::createConcrete(1)));
 
-    TEST_LOG << "Test non-empty ConstantTable#contains";
     REQUIRE(table.contains(field1));
     REQUIRE(table.contains(field3));
     REQUIRE_FALSE(table.contains(field2));
+}
+
+TEST_CASE("ConstantTable getSize") {
+    ConstantTable table{};
+    PKBField field1 = PKBField::createConcrete(1);
+    REQUIRE(table.getSize() == 0);
+
+    table.insert(field1);
+    REQUIRE(table.getSize() == 1);
+}
+
+TEST_CASE("ConstantTable getAllConst") {
+    ConstantTable table{};
+    std::vector<CONST> expected{};
+    REQUIRE(expected == table.getAllConst());
+
+    table.insert(PKBField::createConcrete(1));
+    table.insert(PKBField::createConcrete(2));
+    expected = { 1, 2 };
+    REQUIRE(expected == table.getAllConst());
 }
