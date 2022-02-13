4c18964abaa86a7ae8024b383b7b32d14414c9f2
bailin <rkxuanfeng@gmail.com>
2022-02-13 13:19:06 +0800

    Add follows relationship extractor

--- Team05/Code05/src/unit_testing/src/TestDesignExtractor.cpp
+++ Team05/Code05/src/unit_testing/src/TestDesignExtractor.cpp
@@ -11,6 +11,7 @@
 #include "DesignExtractor/EntityExtractor/VariableExtractor.h"
 #include "DesignExtractor/RelationshipExtractor/ModifiesExtractor.h"
 #include "DesignExtractor/RelationshipExtractor/UsesExtractor.h"
+#include "DesignExtractor/RelationshipExtractor/FollowsExtractor.h"
 #include "PKB.h"
 #include "logging.h"
 
@@ -223,6 +224,23 @@ namespace AST {
                 };
 
                 REQUIRE(pkbStrategy.relationships[PKBRelationship::USES] == expected);
+            }
+        
+            SECTION("Follows extractor test") {
+                auto fe = std::make_shared<FollowsExtractor>(&pkbStrategy);
+                program->accept(fe);
+
+                std::set<std::pair<Content, Content>> expected = {
+                    p(STMT_LO{1, StatementType::Read}, STMT_LO{2, StatementType::Assignment}),
+                    p(STMT_LO{2, StatementType::Assignment}, STMT_LO{3, StatementType::While}),
+                    p(STMT_LO{3, StatementType::While}, STMT_LO{11, StatementType::Print}),
+                    p(STMT_LO{4, StatementType::Print}, STMT_LO{5, StatementType::Assignment}),
+                    p(STMT_LO{5, StatementType::Assignment}, STMT_LO{6, StatementType::Assignment}),
+                    p(STMT_LO{6, StatementType::Assignment}, STMT_LO{7, StatementType::If}),
+                    p(STMT_LO{7, StatementType::If}, STMT_LO{10, StatementType::Assignment}),
+                };
+                auto diff = setDiff(pkbStrategy.relationships[PKBRelationship::FOLLOWS], expected);
+                REQUIRE(pkbStrategy.relationships[PKBRelationship::FOLLOWS] == expected);
             }
         }
     }
