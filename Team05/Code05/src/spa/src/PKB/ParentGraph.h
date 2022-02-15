d31fc4c99f21acfc682a5ff626abd582b34f40a5
Patrick <patricktan9955@hotmail.com>
2022-02-15 16:23:46 +0800

    Fix linting errors

--- Team05/Code05/src/spa/src/PKB/ParentGraph.h
+++ Team05/Code05/src/spa/src/PKB/ParentGraph.h
@@ -1,24 +1,24 @@
-//#pragma once
+// #pragma once
 //
-//#include <map>
-//#include <vector>
-//#include <set>
-//#include <unordered_set>
+// #include <map>
+// #include <vector>
+// #include <set>
+// #include <unordered_set>
 //
-//#include "PKBField.h"
+// #include "PKBField.h"
 //
-//using Result = std::unordered_set<std::vector<PKBField>, PKBFieldVectorHash>;
-//using EdgeSet = std::set<STMT_LO>;
+// using Result = std::unordered_set<std::vector<PKBField>, PKBFieldVectorHash>;
+// using EdgeSet = std::set<STMT_LO>;
 //
-//class ParentGraph {
-//public:
-//    void addEdge(STMT_LO u, STMT_LO v);
-//    Result getParentT(PKBField entity1, PKBField entity2);
+// class ParentGraph {
+// public:
+//     void addEdge(STMT_LO u, STMT_LO v);
+//     Result getParentT(PKBField entity1, PKBField entity2);
 //
-//private:
-//    std::map<STMT_LO, EdgeSet> edges;
-//    std::map<STMT_LO, STMT_LO> reversedEdges;
-//    Result traverseStartT(PKBField entity1, PKBField entity2);
-//    Result traverseEndT(PKBField entity1, PKBField entity2);
-//    // TODO(pattan): May need additional helper functions
+// private:
+//     std::map<STMT_LO, EdgeSet> edges;
+//     std::map<STMT_LO, STMT_LO> reversedEdges;
+//     Result traverseStartT(PKBField entity1, PKBField entity2);
+//     Result traverseEndT(PKBField entity1, PKBField entity2);
+//     // TODO(pattan): May need additional helper functions
 //};
