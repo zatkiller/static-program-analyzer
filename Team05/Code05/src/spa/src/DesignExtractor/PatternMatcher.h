0d59053634173b4c12c4b4e3cfba91be1acb2dc7
bailin <rkxuanfeng@gmail.com>
2022-02-17 02:13:54 +0800

    Add documentation to pattern matcher

--- Team05/Code05/src/spa/src/DesignExtractor/PatternMatcher.h
+++ Team05/Code05/src/spa/src/DesignExtractor/PatternMatcher.h
@@ -13,4 +13,15 @@
 using PatternParam = std::optional<std::string>;
 using AssignPatternReturn = std::list<std::reference_wrapper<const AST::Assign>>;
 
+/**
+ * @brief Extracts all assignment statements in AST with given root that satisfy the constraint given.
+ * 
+ * Currently it does not support strict matching like pattern a(v, "x")
+ * Only partial matching like pattern a(v, _"x+1"_) is supported.
+ * 
+ * @param root The root of the AST
+ * @param lhs The optional string constraint of LHS variable being assigned. Use std::nullopt if LHS is wildcard or synonym. 
+ * @param rhs The optional string constraint of RHS expression being used. Use std::nullopt if RHS  is wildcard.
+ * @return AssignPatternReturn return a list of references to the assign nodes.
+ */
 AssignPatternReturn extractAssign(AST::ASTNode *root, PatternParam lhs, PatternParam rhs);
