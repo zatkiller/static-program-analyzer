1c5b64c621025e4c30379d495e972ae8172b2b0f
Teo Jun Xiong <teojunxiong97@gmail.com>
2022-02-17 15:52:30 +0800

    Change methods of ProcedureTable to strings

--- Team05/Code05/src/unit_testing/src/TestProcedureTable.cpp
+++ Team05/Code05/src/unit_testing/src/TestProcedureTable.cpp
@@ -4,48 +4,26 @@
 #include "PKB/PKBField.h"
 #include "catch.hpp"
 
-TEST_CASE("ProcedureTable testing") {
-    ProcedureTable table{};
-
-    PKBField field1 = PKBField::createConcrete(PROC_NAME{ "main" });
-    PKBField field2 = PKBField::createConcrete(PROC_NAME{ "foo" });
-
-    REQUIRE_FALSE(table.contains(field1));
-
-    table.insert(field1);
-    table.insert(field1);
-    REQUIRE(table.getSize() == 1);
-
-    REQUIRE(table.contains(field1));
-    REQUIRE_FALSE(table.contains(field2));
-}
-
 TEST_CASE("ProcedureTable insert and contains") {
     ProcedureTable table{};
-    PKBField field1 = PKBField::createConcrete(PROC_NAME{ "main" });
-    PKBField field2 = PKBField::createConcrete(PROC_NAME{ "foo" });
-    PKBField field3 = PKBField::createConcrete(PROC_NAME{ "main" });
+    REQUIRE_FALSE(table.contains("main"));
 
-    REQUIRE_FALSE(table.contains(field1));
+    table.insert("main");
+    table.insert("main");
+    REQUIRE(table.contains("main"));
 
-    table.insert(field1);
-    table.insert(field3);
-    REQUIRE(table.contains(PKBField::createConcrete(PROC_NAME{ "main" })));
-
-    REQUIRE(table.contains(field1));
-    REQUIRE(table.contains(field3));
-    REQUIRE_FALSE(table.contains(field2));
+    REQUIRE(table.contains("main"));
+    REQUIRE_FALSE(table.contains("foo"));
 }
 
 TEST_CASE("ProcedureTable getSize") {
     ProcedureTable table{};
-    PKBField field1 = PKBField::createConcrete(PROC_NAME{ "main" });
     REQUIRE(table.getSize() == 0);
 
-    table.insert(field1);
+    table.insert("main");
     REQUIRE(table.getSize() == 1);
 
-    table.insert(field1);
+    table.insert("main");
     REQUIRE(table.getSize() == 1);
 }
 
@@ -54,8 +32,8 @@ TEST_CASE("ProcedureTable getAllProcs") {
     std::vector<PROC_NAME> expected{};
     REQUIRE(expected == table.getAllProcs());
 
-    table.insert(PKBField::createConcrete(PROC_NAME{ "main" }));
-    table.insert(PKBField::createConcrete(PROC_NAME{ "foo" }));
+    table.insert("main");
+    table.insert("foo");
     expected = { PROC_NAME{"foo"}, PROC_NAME{"main"} };
     REQUIRE(expected == table.getAllProcs());
 }
