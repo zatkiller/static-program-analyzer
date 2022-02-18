099eafe9fe0099b61234506cb0a1cf0699a7a129
Teo Jun Xiong <teojunxiong97@gmail.com>
2022-02-18 20:07:27 +0800

    Implement PKB::match

--- Team05/Code05/src/spa/src/DesignExtractor/PatternMatcher.h
+++ Team05/Code05/src/spa/src/DesignExtractor/PatternMatcher.h
@@ -2,13 +2,8 @@
 
 #include <list>
 #include <optional>
-#include <algorithm>
 
-#include "DesignExtractor.h"
 #include "Parser/AST.h"
-#include "Parser/Lexer.h"
-#include "Parser/Parser.h"
-#include "PKB/PKBDataTypes.h"
 
 using PatternParam = std::optional<std::string>;
 using AssignPatternReturn = std::list<std::reference_wrapper<const AST::Assign>>;
