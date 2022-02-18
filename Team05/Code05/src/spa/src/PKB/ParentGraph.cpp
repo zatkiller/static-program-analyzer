bfee863ca8846a22e0723238489386d9e6503702
Patrick <patricktan9955@hotmail.com>
2022-02-19 00:45:42 +0800

    Implement ParentGraph with recursive lookup functionality

--- Team05/Code05/src/spa/src/PKB/ParentGraph.cpp
+++ Team05/Code05/src/spa/src/PKB/ParentGraph.cpp
@@ -1,1 +1,312 @@
-// #include "ParentGraph.h"
+#include "ParentGraph.h"
+
+void ParentGraph::addEdge(STMT_LO u, STMT_LO v) {
+    ParentNode* uNode;
+    ParentNode* vNode;
+
+    // Statement's parent cannot come after the statement
+    if (u.statementNum >= v.statementNum) {
+        return;
+    }
+
+    // First stmt has to be a container statement
+    if (u.type.value() != StatementType::If || u.type.value() != StatementType::While) {
+        return;
+    }
+
+    // Filter nodes whose statement numbers are the same as inputs
+    std::map<STMT_LO, ParentNode*> filtered;
+    std::copy_if(nodes.begin(), nodes.end(), std::back_inserter(filtered),
+        [u, v](decltype(nodes)::value_type const& pair) {
+            return pair.first.statementNum == u.statementNum ||
+                pair.first.statementNum == v.statementNum;
+        });
+
+    bool hasExistingStatementNo = false;
+    for (auto [stmt, node] : filtered) {
+        
+        // Case where a node for u already exists
+        if (stmt == u) {
+            uNode = nodes.at(u);
+            hasExistingStatementNo = true;
+            break;
+        }
+
+        // Invalid insert
+        if (stmt.statementNum == u.statementNum && stmt.type.value() != u.type.value()) {
+            return;
+        }
+    }
+
+    if (!hasExistingStatementNo) {
+        uNode = new ParentNode(u, nullptr, ParentNode::NodeSet{});
+        nodes.emplace(u, uNode);
+    }
+
+    hasExistingStatementNo = false;
+    for (auto [stmt, node] : filtered) {
+        
+        // Case where a node for v already exists
+        if (stmt == v) {
+            vNode = nodes.at(v);
+            hasExistingStatementNo = true;
+            break;
+        }
+
+        // Invalid insert
+        if (stmt.statementNum == v.statementNum && stmt.type.value() != v.type.value()) {
+            return;
+        }
+    }
+
+    if (!hasExistingStatementNo) {
+        vNode = new ParentNode(v, nullptr, ParentNode::NodeSet{});
+        nodes.emplace(v, vNode);
+    }
+
+    // Add the edge between uNode and vNode
+    uNode->next.push_back(vNode);
+    if (!vNode->prev) {
+        vNode->prev = uNode;
+    }
+}
+
+bool ParentGraph::getContains(PKBField field1, PKBField field2) {
+    STMT_LO parent = *field1.getContent<STMT_LO>();
+    STMT_LO child = *field2.getContent<STMT_LO>();
+
+    if (nodes.count(parent) != 0) {
+        ParentNode* curr = nodes.at(parent);
+        ParentNode::NodeSet nextNodes = curr->next;
+        ParentNode::NodeSet filtered;
+        std::copy_if(nextNodes.begin(), nextNodes.end(),
+            std::back_inserter(filtered), [child](ParentNode* node) {
+                return node->stmt == child;
+            });
+        return (filtered.size() == 1);
+    }
+    return false;
+}
+
+bool ParentGraph::getContainsT(PKBField field1, PKBField field2) {
+    STMT_LO parent = *field1.getContent<STMT_LO>();
+    STMT_LO child = *field2.getContent<STMT_LO>();
+
+    // Base Case where Parent(field1, field2) holds
+    if (this->getContains(field1, field2)) {
+        return true;
+    }
+
+    if (nodes.count(parent) != 0) {
+        ParentNode* curr = nodes.at(parent);
+        ParentNode::NodeSet nextNodes = curr->next;
+
+        // Recursive lookup for each node in the NodeSet
+        for (auto node : nextNodes) {
+            STMT_LO newStmt = node->stmt;
+            PKBField newField1 = PKBField::createConcrete(Content{ newStmt });
+            if (this->getContainsT(newField1, field2)) {
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+Result ParentGraph::getParent(PKBField field1, PKBField field2) {
+    bool isConcreteFirst = field1.isValidConcrete(PKBEntityType::STATEMENT);
+    bool isConcreteSec = field2.isValidConcrete(PKBEntityType::STATEMENT);
+
+    if (isConcreteFirst && isConcreteSec) {
+        return this->getContains(field1, field2) 
+            ? Result{ {{field1, field2}} } 
+            : Result{};
+    } else if (isConcreteFirst && !isConcreteSec) {
+        STMT_LO target = *field2.getContent<STMT_LO>();
+        StatementType targetType = target.type.value_or(StatementType::All);
+        Result res{};
+
+        if (nodes.count(target) != 0) {
+            ParentNode* curr = nodes.at(target);
+
+            if (!curr->next.empty()) {
+                ParentNode::NodeSet nextNodes = curr->next;
+                ParentNode::NodeSet filtered;
+                std::copy_if(nextNodes.begin(), nextNodes.end(), std::back_inserter(filtered),
+                    [targetType](ParentNode *node) {
+                        return (node->stmt.type.value() == targetType || targetType == StatementType::All);
+                    });
+                for (ParentNode *node : filtered) {
+                    std::vector<PKBField> temp;
+                    PKBField second = PKBField::createConcrete(Content{ node->stmt });
+                    temp.push_back(field1);
+                    temp.push_back(second);
+                    res.insert(temp);
+                }
+            }
+        }
+        return res;
+    } else if (!isConcreteFirst && isConcreteSec) {
+        STMT_LO target = *field1.getContent<STMT_LO>();
+        StatementType targetType = target.type.value_or(StatementType::All);
+        Result res{};
+
+        if (nodes.count(target) != 0) {
+            ParentNode* curr = nodes.at(target);
+
+            if (!curr->next.empty()) {
+                ParentNode::NodeSet nextNodes = curr->next;
+                ParentNode::NodeSet filtered;
+                std::copy_if(nextNodes.begin(), nextNodes.end(), std::back_inserter(filtered),
+                    [targetType](ParentNode* node) {
+                        return (node->stmt.type.value() == targetType || targetType == StatementType::All);
+                    });
+                for (ParentNode* node : filtered) {
+                    std::vector<PKBField> temp;
+                    PKBField first = PKBField::createConcrete(Content{ node->stmt });
+                    temp.push_back(first);
+                    temp.push_back(field2);
+                    res.insert(temp);
+                }
+            }
+        }
+    } else {
+        return this->traverseAll(field1.statementType.value(), field2.statementType.value());
+    }
+}
+
+Result ParentGraph::getParentT(PKBField field1, PKBField field2) {
+    bool isConcreteFirst = field1.fieldType == PKBFieldType::CONCRETE;
+    bool isDeclarationFirst = field1.fieldType == PKBFieldType::DECLARATION;
+    bool isConcreteSec = field2.fieldType == PKBFieldType::CONCRETE;
+    bool isDeclarationSec = field2.fieldType == PKBFieldType::DECLARATION;
+
+    if (isConcreteFirst && isDeclarationSec) {
+        return traverseStartT(field1, field2);
+    } else if (isDeclarationFirst && isConcreteSec) {
+        return traverseEndT(field1, field2);
+    } else if (isDeclarationFirst && isDeclarationSec) {
+        return traverseAllT(field1.statementType.value(), field2.statementType.value());
+    } else {
+        return Result{};
+    }
+}
+
+Result ParentGraph::traverseStartT(PKBField field1, PKBField field2) {
+    std::set<STMT_LO> found;
+    Result res{};
+    StatementType targetType = field2.statementType.value();
+    STMT_LO startStmt = *(field1.getContent<STMT_LO>());
+
+    if (nodes.count(startStmt) != 0) {
+        traverseStartT(&found, nodes.at(startStmt), targetType);
+    }
+
+    if (!found.empty()) {
+        std::transform(found.begin(), found.end(), std::insert_iterator<Result>(res, std::next(res.begin())),
+            [field1](STMT_LO stmt) {
+                PKBField second = PKBField::createConcrete(Content{ stmt });
+                return std::vector<PKBField>{field1, second};
+            });
+    }
+    
+    return res;
+}
+
+void ParentGraph::traverseStartT(std::set<STMT_LO>* found, ParentNode* node, StatementType targetType) {
+    ParentNode::NodeSet nextNodes = node->next;
+    
+    if (!nextNodes.empty()) {
+        for (auto nextNode : nextNodes) {
+            bool typeMatch = nextNode->stmt.type.value() == targetType || targetType == StatementType::All;
+            if (typeMatch) {
+                found->insert(nextNode->stmt);
+            }
+            traverseStartT(found, nextNode, targetType);
+        }
+    }
+    return;
+}
+
+Result ParentGraph::traverseEndT(PKBField field1, PKBField field2) {
+    std::set<STMT_LO> found;
+    Result res{};
+    StatementType targetType = field1.statementType.value();
+    STMT_LO startStmt = *(field2.getContent<STMT_LO>());
+
+    if (nodes.count(startStmt) != 0) {
+        traverseEndT(&found, nodes.at(startStmt), targetType);
+    }
+
+    if (!found.empty()) {
+        std::transform(found.begin(), found.end(), std::insert_iterator<Result>(res, std::next(res.begin())),
+            [field2](STMT_LO stmt) {
+                PKBField first = PKBField::createConcrete(Content{ stmt });
+                return std::vector<PKBField>{first, field2};
+            });
+    }
+
+    return res;
+}
+
+Result ParentGraph::traverseEndT(std::set<STMT_LO>* found, ParentNode* node, StatementType targetType) {
+    while (node->prev) {
+        bool typeMatch = node->prev->stmt.type.value() == targetType || targetType == StatementType::All;
+        if (typeMatch) {
+            found->insert(node->prev->stmt);
+        }
+        node = node->prev;
+    }
+}
+
+Result ParentGraph::traverseAllT(StatementType type1, StatementType type2) {
+    std::set<STMT_LO> found;
+    Result res;
+
+    for (auto const& [stmt_lo, node] : nodes) {
+        ParentNode* curr = node;
+        found.clear();
+        
+        bool typeMatch = type1 == StatementType::All || node->stmt.type.value() == type1;
+        if (typeMatch) {
+            traverseStartT(&found, node, type2);
+        }
+
+        std::transform(found.begin(), found.end(), std::insert_iterator<Result>(res, std::next(res.begin())), 
+            [curr](STMT_LO stmt) {
+                PKBField first = PKBField::createConcrete(Content{ curr->stmt });
+                PKBField second = PKBField::createConcrete(Content{ stmt });
+                return std::vector<PKBField>{first, second};
+            });
+    }
+
+    return res;
+}
+
+Result ParentGraph::traverseAll(StatementType type1, StatementType type2) {
+    Result res{};
+    for (auto const& [stmt_lo, node] : nodes) {
+        ParentNode* curr = node;
+        bool typeMatch1 = curr->stmt.type.value() == type1 || type1 == StatementType::All;
+
+        if (!curr->next.empty() && typeMatch1) {
+            ParentNode::NodeSet nextNodes = curr->next;
+
+            // Filter nodes that match second statement type
+            ParentNode::NodeSet filtered;
+            std::copy_if(nextNodes.begin(), nextNodes.end(), std::back_inserter(filtered),
+                [type2](ParentNode* node) {
+                    return node->stmt.type.value() == type2 || type2 == StatementType::All;
+                });
+
+            // Populate result
+            std::transform(filtered.begin(), filtered.end(), std::insert_iterator<Result>(res, std::next(res.begin())),
+                [curr](ParentNode* node) {
+                    PKBField first = PKBField::createConcrete(Content{ curr->stmt });
+                    PKBField second = PKBField::createConcrete(Content{ node->stmt });
+                    return std::vector<PKBField>{first, second};
+                });
+        }
+    }
+    return res;
+}
--- Team05/Code05/src/spa/src/PKB/ParentGraph.h
+++ Team05/Code05/src/spa/src/PKB/ParentGraph.h
@@ -1,24 +1,39 @@
-// #pragma once
-//
-// #include <map>
-// #include <vector>
-// #include <set>
-// #include <unordered_set>
-//
-// #include "PKBField.h"
-//
-// using Result = std::unordered_set<std::vector<PKBField>, PKBFieldVectorHash>;
-// using EdgeSet = std::set<STMT_LO>;
-//
-// class ParentGraph {
-// public:
-//     void addEdge(STMT_LO u, STMT_LO v);
-//     Result getParentT(PKBField field1, PKBField field2);
-//
-// private:
-//     std::map<STMT_LO, EdgeSet> edges;
-//     std::map<STMT_LO, STMT_LO> reversedEdges;
-//     Result traverseStartT(PKBField field1, PKBField field2);
-//     Result traverseEndT(PKBField field1, PKBField field2);
-//     // TODO(pattan): May need additional helper functions
-//};
+#pragma once
+
+#include <map>
+#include <vector>
+#include <set>
+#include <algorithm>
+#include <unordered_set>
+#include <iterator>
+
+#include "PKBField.h"
+
+using Result = std::unordered_set<std::vector<PKBField>, PKBFieldVectorHash>;
+
+struct ParentNode {
+    using NodeSet = std::vector<ParentNode*>;
+    ParentNode(STMT_LO stmt, ParentNode* prev, NodeSet next) : stmt(stmt), next(next), prev(prev) {}
+
+    STMT_LO stmt;
+    NodeSet next;
+    ParentNode *prev;
+};
+
+class ParentGraph {
+public:
+    void addEdge(STMT_LO u, STMT_LO v);
+    bool getContains(PKBField field1, PKBField field2);
+    bool getContainsT(PKBField field1, PKBField field2);
+    Result getParent(PKBField field1, PKBField field2);
+    Result getParentT(PKBField field1, PKBField field2);
+
+private:
+    std::map<STMT_LO, ParentNode*> nodes;
+    Result traverseStartT(PKBField field1, PKBField field2);
+    void traverseStartT(std::set<STMT_LO>* found, ParentNode* node, StatementType targetType);
+    Result traverseEndT(PKBField field1, PKBField field2);
+    Result traverseEndT(std::set<STMT_LO>* found, ParentNode* node, StatementType targetType);
+    Result traverseAllT(StatementType field1, StatementType field2);
+    Result traverseAll(StatementType type1, StatementType type2);
+};
