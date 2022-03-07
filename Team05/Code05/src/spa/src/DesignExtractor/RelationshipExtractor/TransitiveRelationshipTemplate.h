bcddd488e35d30496640ad1419c6cd882a7b2cda
bailin <rkxuanfeng@gmail.com>
2022-03-07 19:58:56 +0800

    Add documentation for transitive relationship extractors

--- Team05/Code05/src/spa/src/DesignExtractor/RelationshipExtractor/TransitiveRelationshipTemplate.h
+++ Team05/Code05/src/spa/src/DesignExtractor/RelationshipExtractor/TransitiveRelationshipTemplate.h
@@ -27,6 +27,10 @@ protected:
  * The base abstract template class that handles transitive properties of relationships.
  * 
  * The transitive relationship is derived by cascading up the container stack built from AST DFS traversal.
+ * 
+ * Call statements can be considered a deferred container for an external procedure. However, due to the limitation
+ * of the tree walking implementation, such deferred result cannot be captured naturally. Therefore, a preprocessing 
+ * is required to extract procedures in topological order for correct extractions.
  *  
  * Currently implemented by UsesExtractor and ModifiesExtractor.
  */
@@ -34,8 +38,16 @@ class TransitiveRelationshipTemplate : public RelExtractorTemplate {
 public:
     using RelExtractorTemplate::RelExtractorTemplate;
     void visit(const ast::Call&) override;
+    /**
+     * @brief Preprocess the given AST to create a topological order of procedure to be extracted.
+     * 
+     * Extracting by the topological order of procedures, the variables of all procedures will be extracted before
+     * they are called by other procedures.
+     * 
+     * @param node the starting node for extraction.
+     */
     void extract(ast::ASTNode *node) override;
-    
+
 protected:
     std::map<int, StatementType> stmtNumToType;
 
