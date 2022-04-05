7e909a253f45bccd5bd9d0f07a69bca2f693810d
Nay Lin Han <jamesliu09@gmail.com>
2022-04-05 19:54:43 +0800

    Update Extractors to extract CFG once

--- Team05/Code05/src/spa/src/SourceProcessor.cpp
+++ Team05/Code05/src/spa/src/SourceProcessor.cpp
@@ -10,18 +10,29 @@ bool SourceProcessor::processSimple(const std::string& sourceCode, PKB *pkb) {
     using sp::design_extractor::ActualPKBStrategy;
     using sp::design_extractor::DesignExtractor;
 
+    // parsing source code and extracting AST
     auto ast = sp::parser::parse(sourceCode);
     // parsing failed
     if (!ast) {
         return false;
     }
 
-    ActualPKBStrategy actualPKBStrategy(pkb);
-    DesignExtractor(&actualPKBStrategy).extract(ast.get());
+    // extracting CFG
     sp::cfg::CFGExtractor cfgExtractor;
-    sp::cfg::PROC_CFG_MAP cfg = cfgExtractor.extract(ast.get());
-    pkb->insertCFG(cfg);
+    sp::cfg::PROC_CFG_MAP cfgs = cfgExtractor.extract(ast.get());
+
+    // setting up Design Extractor
+    ActualPKBStrategy actualPKBStrategy(pkb);
+    auto de = DesignExtractor(&actualPKBStrategy);
+    de.insert(cfgs);  // insert CFG to Design Extractor for Next Extractor
+    
+    // running extractors and inserting into PKB
+    de.extract(ast.get());
+
+    // inserting CFG and AST into PKB
+    pkb->insertCFG(cfgs);
     pkb->insertAST(std::move(ast));
+    
     return true;
 }
 
